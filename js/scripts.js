10270ed56bfa8aeac6daad33c89e23df1a8215cc
Vitaliy Sulimenko <prevetal@gmail.com>
2023-03-07 17:40:25 +0100

    Добавили калькулятор

--- js/scripts.js
+++ js/scripts.js
@@ -270,6 +270,141 @@ $(window).on('load', () => {
 				}
 			})
 	}
+
+
+	// Кастомный select
+	const selects = document.querySelectorAll('select')
+
+	if (selects) {
+		selects.forEach(el => NiceSelect.bind(el))
+	}
+
+
+	// Калькулятор
+	var costPerMonth = $('.calc #cost_per_month').ionRangeSlider({
+		min: 105000,
+		max: 155000,
+		from: 105000,
+		step: 100,
+		postfix: ' ₽',
+		onChange: data => {
+			candidateCostCalc()
+			youSaveCalc()
+		}
+	}).data('ionRangeSlider')
+
+	var HRSalary = $('.calc #HR_salary').ionRangeSlider({
+		min: 80000,
+		max: 150000,
+		from: 80000,
+		step: 100,
+		postfix: ' ₽',
+		onChange: data => {
+			candidateCostCalc()
+			youSaveCalc()
+		}
+	}).data('ionRangeSlider')
+
+	var additionalHRSalary = $('.calc #additional_HR_salary').ionRangeSlider({
+		min: 0,
+		max: 90000,
+		from: 0,
+		step: 100,
+		postfix: ' ₽',
+		onChange: data => {
+			candidateCostCalc()
+			youSaveCalc()
+		}
+	}).data('ionRangeSlider')
+
+	var candidateVerifi = $('.calc #candidate_verifi').ionRangeSlider({
+		min: 5000,
+		max: 15000,
+		from: 5000,
+		step: 100,
+		postfix: ' ₽',
+		onChange: data => {
+			candidateCostCalc()
+			youSaveCalc()
+		}
+	}).data('ionRangeSlider')
+
+
+	// Трудозатраты на поиск
+	const laborCostCalc = () => {
+		// Расчёт
+		let result = 0
+
+		// Для руководителя высшего звена другая формула
+		if($('.calc .business select').val() == 4) {
+			result = Math.round(HRSalary.result.from/168*90/1000)*1000
+			$('.calc .labor_costs .label span').text('90')
+		} else {
+			result = Math.round(HRSalary.result.from/168*50/1000)*1000
+			$('.calc .labor_costs .label span').text('50')
+		}
+
+		// Отображение результата
+		$('.calc .labor_costs .price span').text(result.toLocaleString('ru-RU'))
+
+		return result
+	}
+
+	laborCostCalc()
+
+
+	// Итоговые затраты
+	const candidateCostCalc = () => {
+		// Суммирование
+		let result = costPerMonth.result.from + laborCostCalc() + additionalHRSalary.result.from + candidateVerifi.result.from
+
+		// Повторный поиск
+		if($('.calc input[name=repeated_search]').prop('checked')){
+			result = result * 2
+		}
+
+		// Отображение результата
+		$('.calc .candidate_cost .price span').text(result.toLocaleString('ru-RU'))
+
+		return result
+	}
+
+	candidateCostCalc()
+
+
+	// Повторный поиск
+	$('.calc .repeated_search .checkbox label').click(function() {
+		setTimeout(() => {
+			candidateCostCalc()
+			youSaveCalc()
+		})
+	})
+
+
+	// Вы экономите
+	const youSaveCalc = () => {
+		// Расчёт
+		let result = 0
+
+		if($('.calc .candidate_cost .exp.active').length) {
+			result = candidateCostCalc() - parseInt($('.calc .candidate_cost .exp.active .price').data('price'))
+		}
+
+		// Отображение результата
+		$('.calc .bottom .total_price .price span').text(result.toLocaleString('ru-RU'))
+	}
+
+	youSaveCalc()
+
+
+	// Выбор вакансии
+	$('.calc .business select').change(function() {
+		$('.calc .candidate_cost .exp').removeClass('active')
+		$('.calc .candidate_cost .exp' + $(this).val()).addClass('active')
+
+		laborCostCalc()
+		youSaveCalc()
+	})
 })
 
 
